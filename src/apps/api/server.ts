2aa0891fac99df4409b1b81308c1cc49fdeae8ff
Guilherme Gules Moreira <guilhermegules@gmail.com>
2025-07-09 18:14:55 -0300

    feat: create api structure

---
+++ src/apps/api/server.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+import paymentsRoute from "./routes/payments-route";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = fastify({ logger: true });
+
+app.register(paymentsRoute, { prefix: "api" });
+
+app.listen({ port: Number(process.env.PORT) }, (err, address) => {
+  if (err) throw err;
+  console.log(`Server is running :: ${address}`);
+});
