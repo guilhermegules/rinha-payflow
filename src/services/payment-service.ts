79f359be5d68bfb3ecf02a9cc28df102ac470bb4
Guilherme Gules Moreira <guilhermegules@gmail.com>
2025-07-17 16:51:02 -0300

    feat(payment-service): add body validation

--- src/services/payment-service.ts
+++ src/services/payment-service.ts
@@ -1,12 +1,18 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { ProcessablePaymentBodyDTO } from "../dto/processable-payment-body-dto";
 import { enqueuePayment } from "../ports/payment/enqueue-payment";
+import { db } from "../infra/libs/pg";
+import { processablePaymentBodyValidator } from "../dto/validators/processable-payment-body-validator";
 
 export async function processPayment(
   request: FastifyRequest,
   response: FastifyReply
 ) {
   const body = request.body as ProcessablePaymentBodyDTO;
+
+  if (!processablePaymentBodyValidator(body)) {
+    return response.send(422).send({ message: "Invalid body" });
+  }
 
   await enqueuePayment(body);
 
@@ -19,5 +25,6 @@ export async function getPaymentSummary(
   request: FastifyRequest,
   response: FastifyReply
 ) {
+  console.log((await db.query("select * from payments")).rows);
   return Promise.resolve({ ok: true });
 }
